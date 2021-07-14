0867e38f6a1a83f09c515285ef6b52e0c4db7142
Rich Pelavin <rich@reactor8.com>
2021-07-13 18:21:42 -0700

    Update to produce results helpful for stubbing

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
+from typing import List, Tuple, Any, Dict, Optional, cast
 import os
+import sys
 from ruamel.yaml import YAML
 
 import frigate.gen
@@ -10,8 +12,17 @@ if __name__ == "__main__":
     yaml = YAML()
     with open(os.path.join(chartdir, "values.yaml"), "r") as fh:
         values = yaml.load(fh.read())
-    
-    # TODO: picking up comments not working 
-    var_list = list(frigate.gen.traverse(values, defaults_as_json=False))
-    # TODO: simple render as yaml 
-    pprint.pprint(var_list)
+
+    # TODO: picking up comments not working; when working
+    # second element of tuple should sometimes be a non empty string
+    raw_var_list: List[Tuple[str, Optional[str], Any]] = cast(
+        List[Tuple[str, Optional[str], Any]],
+        list(frigate.gen.traverse(values, defaults_as_json=False)),
+    )
+
+    # raw_var_list is all that is needed for helm parser. below is just for stubbing
+    var_list_for_yaml: List[Dict[str, Any]] = [
+        {"name": el[0], "value": el[2]} for el in raw_var_list
+    ]
+
+    yaml.dump(var_list_for_yaml, sys.stdout)
